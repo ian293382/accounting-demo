67c7abe0c479a41ce3673ce8620ff8537e60acbe
YuYuan <yuyuan@YuYuande-MacBook-Pro.local>
2023-09-22 07:02:36 +0800

    static setting

--- accounting/settings.py
+++ accounting/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_URL = "static/"
 
 # Default primary key field type
