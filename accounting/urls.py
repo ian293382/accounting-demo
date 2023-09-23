789c034607a559967b072cb9b659886ce426abb0
Iannaan <moshou5571228@gmail.com>
2023-09-23 12:25:22 +0800

    change user urls /users/

--- accounting/urls.py
+++ accounting/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('user/',include('user.urls')),
+    path('users/',include('user.urls')),
     path("admin/", admin.site.urls),
   
     
